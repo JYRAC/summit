8a563ca20aa428f6add403d1b243fa23ca4f220b
Takaesu._.p <zinn.1019t@gmail.com>
2026-09-11 01:58:08 +0900

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import unocss from 'unocss/astro';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://summit.jyrac.stki.org',
   integrations: [
     unocss({ mode: 'per-module' }),
+    sitemap(),
   ],
 });
-
